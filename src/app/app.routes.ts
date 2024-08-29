e960b943b3d34df0fb55ccf3e8341585e9a33b56
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-29 14:08:48 -0300

    feat: colocando opção de deletar contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,24 @@
 import { Routes } from '@angular/router';
 import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
 import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
+import { PerfilContatoComponent } from './paginas/perfil-contato/perfil-contato.component';
 
 export const routes: Routes = [
   {
     path: 'formulario',
-    component: FormularioContatoComponent
+    component: FormularioContatoComponent,
   },
   {
     path: 'lista-contatos',
-    component: ListaContatosComponent
+    component: ListaContatosComponent,
+  },
+  {
+    path: 'perfil-contato/:id',
+    component: PerfilContatoComponent,
   },
   {
     path: '',
     redirectTo: '/lista-contatos',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
