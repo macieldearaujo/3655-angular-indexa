00c9cb943ce96b8ea5cc4641e21bd9612c14daa2
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-30 12:11:36 -0300

    feat: editando contato

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -14,10 +14,6 @@ export class ContatoService {
     return this.http.get<Contato[]>(this.API);
   }
 
-  salvarContato(contato: Contato): Observable<Contato> {
-    return this.http.post<Contato>(this.API, contato);
-  }
-
   buscarPorId(id: number): Observable<Contato> {
     const url = `${this.API}/${id}`;
     return this.http.get<Contato>(url);
@@ -28,16 +24,20 @@ export class ContatoService {
     return this.http.delete<Contato>(url);
   }
 
-  editarContato(contato: Contato): Observable<Contato> {
-    const url = `${this.API}/${contato.id}`;
-    return this.http.put<Contato>(this.API, contato);
+  salvarContato(contato: Contato): Observable<Contato> {
+    return this.http.post<Contato>(this.API, contato);
   }
 
-  editarOuSalvar(contato: Contato) {
+  editarContato(contato: Contato): Observable<Contato> {
+    const url = `${this.API}/${contato.id}`;
+    return this.http.put<Contato>(url, contato);
+  }
+
+  editarOuSalvarContato(contato: Contato): Observable<Contato> {
     if (contato.id) {
-      return this.salvarContato(contato);
-    } else {
       return this.editarContato(contato);
+    } else {
+      return this.salvarContato(contato);
     }
   }
 }
